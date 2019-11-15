a7de51c212ccc7c1e95764460e394f203b510a25
thydmle <tle3@ithaca.edu>
2019-11-15 13:43:29 -0500

    discard_crib for greedycribbageagent

--- cribbage/Agents.py
+++ cribbage/Agents.py
@@ -42,44 +42,17 @@ class GreedyCribbageAgent:
     def __init__(self):
         self.score = 0
 
-    def find_pair(self, hand):
+    def discard_crib(self, hand, is_dealer):
         """
         Thy
-        Looks through hand, finds possible pairs
-        :param hand: a list of tuples, each tuple contains the suit of the card and the value of the card
-        :return: a set of pairs of cards, ordered next to one another
+        :param hand: list of 6 cards, each card is a tuple of value and suit
+        :param is_dealer: whether the player is the dealer
+        :return: the tuple of 2 cards to discard
         """
-        paired = []
-        for current in range(0, len(hand), 1):
-            current_card = hand[current]
-            (current_value, current_suit) = current_card
-            for compare in range(current + 1, len(hand), 1):
-                compare_card = hand[compare]
-                (compare_value, compare_suit) = compare_card
-                if compare_value == current_value:
-                    paired.append(current_card)
-                    paired.append(compare_card)
-                    break
-
-        return paired
-
-    def discard_two(self, hand, index1, index2):
-        """
-        Thy
-        Takes a 6-card hand, removes cards at index1 and index2
-        :param hand: 6-card hand which is a list of tuples, each tuple (1 card) contains the value and the suit of the card
-        :param index1: index of the first card to remove
-        :param index2: index of the second card to remove
-        :return: a 4-card hand without the removed cards,
-                 a 6-card hand IF EITHER of the indices is out of range
-        """
-        if index1 >= len(hand) or index2 >= len(hand):
-            return hand
-
-        else:
-            hand.remove(hand[index1])
-            hand.remove(hand[index2])
-            return hand
+        possible_choices = self.what_if(hand)
+        highest_score_hand = heapq.heappop(possible_choices)
+        (first_index, second_index) = highest_score_hand
+        return hand[first_index], hand[second_index]
 
     def what_if(self, hand):
         """
@@ -89,19 +62,7 @@ class GreedyCribbageAgent:
         :return: a priority queue of 4-card hand based on their scores
         """
         priorityq = []
-        possible_cut_cards = [(1, 1), (1, 2), (1, 3), (1, 4),
-                              (2, 1), (2, 2), (2, 3), (2, 4),
-                              (3, 1), (3, 2), (3, 3), (3, 4),
-                              (4, 1), (4, 2), (4, 3), (4, 4),
-                              (5, 1), (5, 2), (5, 3), (5, 4),
-                              (6, 1), (6, 2), (6, 3), (6, 4),
-                              (7, 1), (7, 2), (7, 3), (7, 4),
-                              (8, 1), (8, 2), (8, 3), (8, 4),
-                              (9, 1), (9, 2), (9, 3), (9, 4),
-                              (10, 1), (10, 2), (10, 3), (10, 4),
-                              (11, 1), (11, 2), (11, 3), (11, 4),
-                              (12, 1), (12, 2), (12, 3), (12, 4),
-                              (13, 1), (13, 2), (13, 3), (13, 4)]
+        possible_cut_cards = [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1), (9, 1), (10, 1), (11, 1), (12, 1), (13, 1)]
         for i in range(len(hand)):
             copyhand = deepcopy(hand)
 
@@ -114,7 +75,7 @@ class GreedyCribbageAgent:
 
                 for cut_card in possible_cut_cards:
                     points = scorer.score_hand(copyhand2, cut_card)
-                    heapq.heappush(priorityq, (points, copyhand2))
+                    heapq.heappush(priorityq, (-points, i, j))
 
         return priorityq
 
