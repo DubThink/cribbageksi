19c2eb90d031913047627e7c52765dc18fc7ddd3
mnoel33 <mnoel@ithaca.edu>
2019-11-18 12:55:20 -0500

    organized score hand function, expected value function

--- cribbage/scoreHand.py
+++ cribbage/scoreHand.py
@@ -3,36 +3,80 @@ from itertools import combinations
 
 def score_hand(hand4cards, cutcard):
     """
-    Returns the point value of a 4 card hand with the given cut card
+    Returns the total point value of a 4 card hand with the given cut card
     :param hand4cards: the 4 cards in the player's hand
     :param cutcard: cut card
     :return: integer point value of the hand
     """
 
-    points = 0
+    total_points = 0
+    total_points += right_jack(hand4cards,cutcard)
+    total_points += flush(hand4cards,cutcard)
+
+    sorted5cards=sort_cards(hand4cards,cutcard)
+
+    total_points += two_card_fifteens(sorted5cards)
+    total_points += three_card_fifteens(sorted5cards)
+    total_points += four_card_fifteens(sorted5cards)
+    total_points += five_card_fifteens(sorted5cards)
+    total_points += runs(sorted5cards)
+    total_points += pairs(sorted5cards)
+
+    return total_points
 
 
-    # right jack
-    for card in hand4cards:
-        if card[0] == 11 and cutcard[1] == card[1]:  # if card in hand is a Jack and its suit matches the cut card
-            points += 1
-
-    # flushes
-    if hand4cards[0][1] == hand4cards[1][1] == hand4cards[2][1] == hand4cards[3][1]:
-        points += 4
-        if hand4cards[0][1] == cutcard[1]:
-            points += 1
-
-
-    # create list of sorted 5 card hand
+def sort_cards(hand4cards,cutcard):
+    """
+    puts the hand of 4 cards and the cut card into one sorted hand
+    :param hand4cards: 4 cards in the player's hand
+    :param cutcard: cut card
+    :return: sorted five card hand
+    """
     hand_queue = []
     hand4cards.append(cutcard)
     for i in range(5):
         heapq.heappush(hand_queue, hand4cards[i])
     sorted5cards = heapq.nsmallest(5, hand_queue)
+    return sorted5cards
 
 
-    #pairs 15's
+def right_jack(hand4cards, cutcard):
+    """
+    Returns the point value from right jacks in the given hand
+    :param hand4cards: the 4 cards in the player's hand
+    :param cutcard: cut card
+    :return: 1 point if the hand contains the right jack, 0 otherwise
+    """
+    points = 0
+    # right jack
+    for card in hand4cards:
+        if card[0] == 11 and cutcard[1] == card[1]:  # if card in hand is a Jack and its suit matches the cut card
+            points += 1
+    return points
+
+
+def flush(hand4cards, cutcard):
+    """
+    Returns the point value from flushes in the given hand
+    :param hand4cards: the 4 cards in the player's hand
+    :param cutcard: cut card
+    :return: points from flushes
+    """
+    points=0
+    # flushes
+    if hand4cards[0][1] == hand4cards[1][1] == hand4cards[2][1] == hand4cards[3][1]:
+        points += 4
+        if hand4cards[0][1] == cutcard[1]:
+            points += 1
+    return points
+
+def two_card_fifteens(sorted5cards):
+    """
+    Returns the point value of pairs of cards that sum to 15
+    :param sorted5cards: sorted list of 4 cards in the player's hand and the cut card
+    :return: points from two card 15's
+    """
+    points=0
     index_combinations2 = combinations([0,1,2,3,4], 2)
     for combination in list(index_combinations2):
         value1 = sorted5cards[combination[0]][0]
@@ -43,8 +87,15 @@ def score_hand(hand4cards, cutcard):
             value2 = 10
         if value1 + value2 == 15:
             points += 2
-            
-    #3 card 15's
+    return points
+
+def three_card_fifteens(sorted5cards):
+    """
+    Returns the point value of 3 cards that sum to 15
+    :param sorted5cards: sorted list of 4 cards in the player's hand and the cut card
+    :return: points from three card 15's
+    """
+    points=0
     index_combinations3 = combinations([0, 1, 2, 3, 4], 3)
     for combination in list(index_combinations3):
         value1 = sorted5cards[combination[0]][0]
@@ -58,8 +109,15 @@ def score_hand(hand4cards, cutcard):
             value3 = 10
         if value1 + value2 + value3 == 15:
             points += 2
+    return points
 
-    # 4 card 15's
+def four_card_fifteens(sorted5cards):
+    """
+    Returns the point value of 4 cards that sum to 15
+    :param sorted5cards: sorted list of 4 cards in the player's hand and the cut card
+    :return: points from four card 15's
+    """
+    points=0
     index_combinations4 = combinations([0, 1, 2, 3, 4], 4)
     for combination in list(index_combinations4):
         value1 = sorted5cards[combination[0]][0]
@@ -76,15 +134,30 @@ def score_hand(hand4cards, cutcard):
             value4 = 10
         if value1 + value2 + value3 + value4 == 15:
             points += 2
+    return points
 
-    # 5 card 15's
+def five_card_fifteens(sorted5cards):
+    """
+    Returns the point value of 5 cards that sum to 15
+    :param sorted5cards: sorted list of 4 cards in the player's hand and the cut card
+    :return: points from five card 15's
+    """
+    points=0
     sum=0
     for i in range(5):
         sum+=sorted5cards[i][0]
     if sum==15:
         points+=2
+    return points
 
-    # runs
+
+def runs(sorted5cards):
+    """
+    Returns the point value from runs
+    :param sorted5cards: sorted list of 4 cards in the player's hand and the cut card
+    :return: points from runs
+    """
+    points=0
     for start_index in range(3):
         next_index=start_index+1
         consecutive_cards_count = 1
@@ -104,16 +177,39 @@ def score_hand(hand4cards, cutcard):
         if consecutive_cards_count >= 3:
             points += multiplier * consecutive_cards_count
             break
+    return points
 
-    # pairs/3 of a kind/4 of a kind
+def pairs(sorted5cards):
+    """
+    Returns the point value from pairs (includes 3 of a kind and 4 of a kind)
+    :param sorted5cards: sorted list of 4 cards in the player's hand and the cut card
+    :return: points from pairs
+    """
+    points=0
     start_card_index = 0
     while start_card_index < 4:
-        index=start_card_index+1
+        index = start_card_index + 1
         for i in range(index, 5):
             if sorted5cards[start_card_index][0] == sorted5cards[i][0]:
                 points += 2
         start_card_index += 1
-
     return points
 
 
+def expected_hand_value(hand4cards,discard2cards):
+    card_counts=[]
+    for i in range(14):
+        card_counts.append(4)
+    six_cards=hand4cards.append(discard2cards[0],discard2cards[1])
+    for card in six_cards:
+        value=card[0]
+        card_counts[value-1] -= 1
+
+    expected_value=0
+
+    for i in range (14):
+        hand_value=score_hand(hand4cards,(i,1))
+        probability=card_counts[i]/46
+        expected_value += (hand_value*probability)
+
+    return expected_value
--- test/scoreHandTest.py
+++ test/scoreHandTest.py
@@ -1,102 +1,70 @@
-from cribbage.scoreHand import score_hand
+from cribbage.scoreHand import score_hand,pairs,two_card_fifteens,three_card_fifteens,four_card_fifteens,five_card_fifteens,runs,right_jack,flush,sort_cards
 import unittest
 
-
 class TestScoreHand(unittest.TestCase):
-    def test_no_points(self):
-        hand=[(4,4),(3,1),(7,1),(12,2)]
-        cut_card=(9,1)
-        self.assertEqual(0, score_hand(hand,cut_card))
-
-    def test_one_pair(self):
-        hand = [(4, 4), (7, 1), (7, 1), (12, 2)]
-        cut_card = (9, 1)
-        self.assertEqual(2, score_hand(hand, cut_card))
-
-    def test_two_pairs(self):
-        hand = [(4, 4), (12, 1), (7, 1), (12, 2)]
-        cut_card = (7, 1)
-        self.assertEqual(4, score_hand(hand, cut_card))
-
-    def test_3_of_a_kind(self):
-        hand = [(4, 4), (4, 1), (4, 3), (12, 2)]
-        cut_card = (6, 1)
-        self.assertEqual(6, score_hand(hand, cut_card))
-
-    def test_4_of_a_kind(self):
-        hand = [(4, 4), (4, 1), (4, 3), (4, 2)]
-        cut_card = (6, 1)
-        self.assertEqual(12, score_hand(hand, cut_card))
 
     def test_right_jack(self):
-        hand = [(2, 4), (11, 1), (8,3), (12, 2)]
-        cut_card = (6, 1)
-        self.assertEqual(1, score_hand(hand, cut_card))
+        hand = [(10, 1), (11, 1), (1, 2), (3, 1)]
+        cutcard = (5, 1)
+        self.assertEqual(1, right_jack(hand,cutcard))
 
-    def test_4_card_flush(self):
-        hand = [(1, 4), (9, 4), (12, 4), (3, 4)]
-        cut_card = (8, 3)
-        self.assertEqual(4, score_hand(hand, cut_card))
+    def test_four_card_flush(self):
+        hand = [(10, 1), (11, 1), (1, 1), (3, 1)]
+        cutcard = (5, 3)
+        self.assertEqual(4, flush(hand,cutcard))
 
-    def test_5_card_flush(self):
-        hand = [(1, 4), (9, 4), (12, 4), (3, 4)]
-        cut_card = (8, 4)
-        self.assertEqual(5, score_hand(hand, cut_card))
+    def test_five_card_flush(self):
+        hand = [(10, 1), (11, 1), (1, 1), (3, 1)]
+        cutcard = (5, 1)
+        self.assertEqual(5, flush(hand,cutcard))
 
-    def test_run_of_3(self):
-        hand = [(1, 4), (2, 1), (10, 3), (8, 2)]
-        cut_card = (9, 2)
-        self.assertEqual(3, score_hand(hand, cut_card))
+    def test_pairs(self):
+        hand = [(12, 1), (11, 4), (11, 2), (12, 3)]
+        cutcard = (11, 1)
+        sorted5cards=sort_cards(hand,cutcard)
+        self.assertEqual(8,pairs(sorted5cards))
 
-    def test_run_of_4(self):
-        hand = [(11, 4), (12, 1), (7, 3), (10, 2)]
-        cut_card = (13, 2)
-        self.assertEqual(4, score_hand(hand, cut_card))
-
-    def test_run_of_5(self):
-        hand = [(11, 4), (12, 1), (9, 3), (10, 2)]
-        cut_card = (13, 2)
-        self.assertEqual(5, score_hand(hand, cut_card))
-
-    def test_one_fifteen(self):
-        hand = [(5, 4), (4, 1), (7, 1), (1, 2)]
-        cut_card = (8, 1)
-        self.assertEqual(2, score_hand(hand, cut_card))
-
-    def test_three_fifteens_and_pair(self):
-        hand = [(5, 4), (7, 1), (7, 1), (12, 2)]
-        cut_card = (8, 1)
-        self.assertEqual(8, score_hand(hand, cut_card))
-
-    def test_three_card_fifteen(self):
-        hand = [(6, 4), (7, 1), (2, 1), (4, 2)]
-        cut_card = (1, 1)
-        self.assertEqual(2, score_hand(hand, cut_card))
-
-    def test_four_card_fifteen(self):
-        hand = [(6, 4), (3, 1), (2, 1), (4, 2)]
-        cut_card = (1, 1)
-        self.assertEqual(6, score_hand(hand, cut_card))
-
-    def test_five_card_fifteen(self):
-        hand = [(10, 4), (1, 1), (2, 1), (1, 2)]
-        cut_card = (1, 3)
-        self.assertEqual(8, score_hand(hand, cut_card))
-
-    def test_double_run(self):
-        hand = [(2, 4), (2, 1), (3, 1), (5, 2)]
-        cut_card = (1, 3)
-        self.assertEqual(8, score_hand(hand, cut_card))
-
-    def test_good_hand(self):
+    def test_runs(self):
         hand = [(10, 1), (11, 1), (11, 2), (12, 1)]
-        cut_card = (5, 1)
-        self.assertEqual(17, score_hand(hand, cut_card))
+        cutcard = (5, 1)
+        sorted5cards=sort_cards(hand,cutcard)
+        self.assertEqual(6,runs(sorted5cards))
+
+    def test_two_card_fifteens(self):
+        hand = [(10, 1), (11, 1), (7, 2), (8, 1)]
+        cutcard = (5, 1)
+        sorted5cards = sort_cards(hand, cutcard)
+        self.assertEqual(6, two_card_fifteens(sorted5cards))
+
+    def test_three_card_fifteens(self):
+        hand = [(4, 1), (3, 1), (4, 2), (8, 1)]
+        cutcard = (5, 1)
+        sorted5cards = sort_cards(hand, cutcard)
+        self.assertEqual(4, three_card_fifteens(sorted5cards))
+
+    def test_four_card_fifteens(self):
+        hand = [(10, 1), (1, 1), (1, 2), (3, 1)]
+        cutcard = (5, 1)
+        sorted5cards = sort_cards(hand, cutcard)
+        self.assertEqual(2, four_card_fifteens(sorted5cards))
+
+    def test_five_card_fifteens(self):
+        hand = [(10, 1), (1, 1), (1, 2), (2, 1)]
+        cutcard = (1, 1)
+        sorted5cards = sort_cards(hand, cutcard)
+        self.assertEqual(2, five_card_fifteens(sorted5cards))
+
+    def test_no_points(self):
+        hand = [(4, 4), (3, 1), (7, 1), (12, 2)]
+        cutcard = (9, 1)
+        self.assertEqual(0,score_hand(hand,cutcard))
 
     def test_best_hand(self):
         hand = [(11, 1), (5, 2), (5, 3), (5, 4)]
-        cut_card = (5, 1)
-        self.assertEqual(29, score_hand(hand, cut_card))
+        cutcard = (5, 1)
+        self.assertEqual(29, score_hand(hand, cutcard))
+
+
 
 if __name__ == '__main__':
     unittest.main()
