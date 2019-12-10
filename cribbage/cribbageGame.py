4e683bf9ba37c8b4edf5d37857a90ecc4571c24b
Ben W <benjaminwelsh2@gmail.com>
2019-12-10 15:51:05 -0500

    documentation

--- cribbage/cribbageGame.py
+++ cribbage/cribbageGame.py
@@ -2,15 +2,15 @@ from cribbage.deck import Deck, card_to_string, peg_val
 from copy import deepcopy
 from cribbage.scoreHand import score_hand
 
-PAIR_SCORES={2:("Pair",2),3:("3 of a kind",6),4:("Four of a kind",12)}
+PAIR_SCORES = {2: ("Pair", 2), 3: ("3 of a kind", 6), 4: ("Four of a kind", 12)}
 
 
 def min_card(hand):
     """
-    Returns a card with the minimum pegging value
+    :returns a card with the minimum pegging value
     """
     c = hand[0]
-    for i in range(1,len(hand)):
+    for i in range(1, len(hand)):
         if peg_val(hand[i]) < peg_val(c):
             c = hand[i]
     return c
@@ -18,18 +18,29 @@ def min_card(hand):
 
 def min_card_val(hand):
     """
-    returns the pegging value of the minimum card
+    :returns the pegging value of the minimum card
     """
     return min([peg_val(c) for c in hand])
 
 
 def can_peg(hand, total):
+    """
+    :returns true if a hand has a card that can be played
+    :param hand: the hand to evaluate
+    :param total: the total score on the table
+    """
     if len(hand) == 0:
         return False
     return min_card_val(hand) + total <= 31
 
 
 class CribbageGame:
+    """
+    A game of cribbage
+    The game is run by calling play_game()
+    The game can only be run once.
+    To disable most of the printout, set game.verbose to false
+    """
     def __init__(self, agent_a, agent_b):
         self.agent_a = agent_a
         self.agent_b = agent_b
@@ -38,6 +49,10 @@ class CribbageGame:
         self.verbose = True
 
     def play_game(self):
+        """
+        The main gameplay function
+        To get the scores post game, check game.a_score and game.b_score
+        """
         a_is_dealer = True
         while not self.run_round(a_is_dealer):
             a_is_dealer = not a_is_dealer
@@ -46,7 +61,7 @@ class CribbageGame:
         """
         Runs a round of cribbage between agent_a and agent_b
         :param a_is_dealer: if a is the dealer and gets the crib
-        :return:
+        :return: true if the game was won, false if otherwise
         """
 
         # setup
@@ -72,7 +87,8 @@ class CribbageGame:
             raise IllegalMoveException("Agent discarded a card it did not own.")
         if discard_b[0] == discard_b[1]:
             raise IllegalMoveException("Agent discarded the same card twice.")
-        crib = [discard_a[0],discard_a[1],discard_b[0],discard_b[1]]
+
+        crib = [discard_a[0], discard_a[1], discard_b[0], discard_b[1]]
         hand_a.remove(discard_a[0])
         hand_a.remove(discard_a[1])
         hand_b.remove(discard_b[0])
@@ -81,15 +97,18 @@ class CribbageGame:
         # cut card
         cut_card = deck.drawCard()
         if self.verbose:
-            print("The cut card is the",card_to_string(cut_card))
+            print("The cut card is the", card_to_string(cut_card))
         if cut_card[0] is 11:  # if the a jack is turned
             self.score_points(2, "His heels", a_is_dealer)
 
+        # run pegging
         if self.pegging(deepcopy(hand_a), deepcopy(hand_b), a_is_dealer):
+            # if the game was won during pegging, return true
             return True
 
         self.print_scores()
-        # print("DEBUG",hand_a,hand_b)
+
+        # score non-dealer's hand
         if a_is_dealer:
             if self.verbose:
                 print("B's hand:")
@@ -104,14 +123,7 @@ class CribbageGame:
         if self.game_over():
             return True
 
-        if self.verbose:
-            print("The crib, which belongs to %s:" % ("A" if a_is_dealer else "B"))
-            print(", ".join([card_to_string(c) for c in crib]))
-        self.score_hand(crib, cut_card, a_is_dealer, True)
-
-        if self.game_over():
-            return True
-
+        # score dealer's hand
         if not a_is_dealer:
             if self.verbose:
                 print("B's hand:")
@@ -126,14 +138,26 @@ class CribbageGame:
         if self.game_over():
             return True
 
+        # score the crib
+        if self.verbose:
+            print("The crib, which belongs to %s:" % ("A" if a_is_dealer else "B"))
+            print(", ".join([card_to_string(c) for c in crib]))
+        self.score_hand(crib, cut_card, a_is_dealer, True)
+
+        if self.game_over():
+            return True
+
         self.print_scores()
 
         return False
 
     def game_over(self):
-        if self.a_score>120:
+        """
+        :returns true if the game was won
+        """
+        if self.a_score > 120:
             winner = "A"
-        elif self.b_score>120:
+        elif self.b_score > 120:
             winner = "B"
         else:
             return False
@@ -146,13 +170,19 @@ class CribbageGame:
         if self.verbose:
             print("Scores:\nA: %d\nB: %d\n" % (self.a_score, self.b_score))
 
-    def score_points(self,amount, reason, is_a):
+    def score_points(self, amount, reason, is_a):
+        """
+        scores points for a player
+        :param amount: the amount of points scored
+        :param reason: the reason for the points as a string. Prints in the format of (amount) for (reason)
+        :param is_a: if the player who scored points was a
+        """
         if is_a:
             self.a_score += amount
         else:
             self.b_score += amount
         if self.verbose:
-            print("%s for %d (%s)"%(reason, amount, "A" if is_a else "B"))
+            print("%s for %d (%s)" % (reason, amount, "A" if is_a else "B"))
 
     def pegging(self, hand_a, hand_b, is_a):
         """
@@ -174,7 +204,8 @@ class CribbageGame:
             if can_peg(hand, total):
                 # a card should be played
                 if pick is None:
-                    raise IllegalMoveException("Must play a card if able to. data:"+str((deepcopy(hand), deepcopy(seq), total))+"   player "+("A" if is_a else "B"))
+                    raise IllegalMoveException("Must play a card if able to. data:" + str(
+                        (deepcopy(hand), deepcopy(seq), total)) + "   player " + ("A" if is_a else "B"))
                 if pick not in hand:
                     raise IllegalMoveException("Must play a card from your hand")
                 if peg_val(pick) + total > 31:
@@ -183,7 +214,7 @@ class CribbageGame:
                 hand.remove(pick)
                 total += peg_val(pick)
                 if self.verbose:
-                    print("%s played %s for %d" % ("A" if is_a else "B",card_to_string(pick),total))
+                    print("%s played %s for %d" % ("A" if is_a else "B", card_to_string(pick), total))
                     # print("total:", total)
                     # print("sequence:", ", ".join([card_to_string(c) for c in seq]))
                 self.score_pegging(seq, total, is_a)
@@ -192,18 +223,18 @@ class CribbageGame:
             else:
                 if pick is not None:
                     raise IllegalMoveException("Played a card that brought the total over 31.")
-            if not can_peg(hand_a,total) and not can_peg(hand_b,total):
+            if not can_peg(hand_a, total) and not can_peg(hand_b, total):
                 # make sure neither player tries to play a card
                 pick_a = player.pegging_move(deepcopy(hand_a), deepcopy(seq), total)
                 pick_b = player.pegging_move(deepcopy(hand_b), deepcopy(seq), total)
                 if pick_a is not None or pick_b is not None:
                     raise IllegalMoveException("Played a card that brought the total over 31.")
                 # neither person can go
-                self.score_points(1,"Last card", is_a)
+                self.score_points(1, "Last card", is_a)
                 if self.game_over():
                     return True
-                total=0
-                seq=[]
+                total = 0
+                seq = []
 
             is_a = not is_a
 
@@ -219,19 +250,19 @@ class CribbageGame:
         :param is_a:
         :return:
         """
-        if len(seq)<2:
+        if len(seq) < 2:
             return
         if total == 15:
-            self.score_points(2,"Fifteen", is_a)
+            self.score_points(2, "Fifteen", is_a)
         if total == 31:
-            self.score_points(1,"Thirty one",is_a)
-        run_up=0
-        run_down=0
-        continue_run=True
-        pair=1
+            self.score_points(1, "Thirty one", is_a)
+        run_up = 0
+        run_down = 0
+        continue_run = True
+        pair = 1
         continue_pair = True
-        top_card=seq[-1]
-        for i in range(len(seq)-2,-1,-1):
+        top_card = seq[-1]
+        for i in range(len(seq) - 2, -1, -1):
             if continue_pair:
                 if seq[i][0] == top_card[0]:
                     pair += 1
@@ -239,28 +270,33 @@ class CribbageGame:
                     continue_pair = False
 
             if continue_run:
-                if seq[i][0]==top_card[0]-1-run_down:
+                if seq[i][0] == top_card[0] - 1 - run_down:
                     # card we're looking at is part of a sequence down
                     run_down += 1
-                elif seq[i][0]==top_card[0]+1+run_up:
+                elif seq[i][0] == top_card[0] + 1 + run_up:
                     run_up += 1
                 else:
                     continue_run = False
         if pair in PAIR_SCORES:
-            self.score_points(PAIR_SCORES[pair][1],PAIR_SCORES[pair][0],is_a)
-        run_total = run_up+run_down+1
-        if run_total>2:
-            self.score_points(run_total,"A run of %d"%run_total,is_a)
+            self.score_points(PAIR_SCORES[pair][1], PAIR_SCORES[pair][0], is_a)
+        run_total = run_up + run_down + 1
+        if run_total > 2:
+            self.score_points(run_total, "A run of %d" % run_total, is_a)
 
         return 0
 
-    def pegging_round(self,hand_a,hand_b,a_goes_first):
+    def pegging_round(self, hand_a, hand_b, a_goes_first):
         pass
 
     def score_hand(self, hand4cards, cutcard, is_a, is_crib=False):
-        self.score_points(score_hand(hand4cards,cutcard,is_crib),"Their cards",is_a)
+        """
+        scores points from a given hand
+        :param hand4cards: the hand's cards
+        :param cutcard: the cut card
+        :return:
+        """
+        self.score_points(score_hand(hand4cards, cutcard, is_crib), "Their cards", is_a)
 
 
 class IllegalMoveException(Exception):
     pass
-
