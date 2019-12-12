2d69e5484ad7647cde64eb822cec81c7df0aa0d7
liam <lkpfaff@gmail.com>
2019-12-12 18:42:35 -0500

    Tree Stuff

--- cribbage/expectimaxTree.py
+++ cribbage/expectimaxTree.py
@@ -3,16 +3,13 @@ from copy import deepcopy
 import cribbage.deck
 
 class node:
-    children = []
-    utility = 0
-    card = None
-    myScore = 0
-    opponantScore = 0
-    sumFromPlay = 0
-
     def __init__(self, card, utility):
+        self.children = []
+        self.utility = 0
+        self.myScore = 0
+        self.opponantScore = 0
+        self.sumFromPlay = 0
         self.card = card
-        self.utility = utility
 
     def addChild(self, childNode):
         self.children.append(childNode)
@@ -23,14 +20,25 @@ class node:
         print("Utility: ", self.utility)
         print("Card:", self.card)
 
+    def getChildren(self):
+        return self.children
+
+    def getCard(self):
+        return self.card
+
+    def getSumFromPlay(self):
+        return self.sumFromPlay
+
+    def setSumFromPlay(self, sum):
+        self.sumFromPlay = sum
+
 
 class chanceNode:
-    children = []
-    utility = 0
-    card = None
-    probability = 0
-
     def __init__(self, card, probability):
+        self.children = []
+        self.utility = 0
+        self.card = card
+        self.probability = probability
         self.utility = 0
 
     def debug(self):
@@ -42,31 +50,35 @@ class chanceNode:
     def addChild(self, childNode):
         self.children.append(childNode)
 
+    def getChildren(self):
+        return self.children
+
+    def getCard(self):
+        return self.card
+
 
 class expectimaxTree:
-    chanceNode = chanceNode
-    node = node
+
 
     # Initializes an expectimax tree of a specified depth
     def __init__(self, hand, sequence, current_sum, tree_depth):
-
         # Initialize tree
         print("hand:", hand)
         print("sequence:", sequence)
         print("current sum:", current_sum)
-        root = self.node(None, 0)
+        root = node(None, 0)
 
         # Consider all cards in hand that are legal moves
         legal_moves = []
         for card in hand:
             if peg_val(card) + current_sum <= 31:
                 legal_moves.append(card)
+                newNode = node(card, 0)
+                newNode.sumFromPlay = current_sum + card[0]
+                root.addChild(newNode)
 
-        # Put valid cards in hand into tree
-        for i in legal_moves:
-            newNode = self.node(i, 0)
-            newNode.sumFromPlay = current_sum + i[0]
-            root.addChild(newNode)
+        print("legal moves: ", legal_moves)
+        print("Root's children: ", root.children)
 
 
         # Get deck of cards
@@ -74,7 +86,7 @@ class expectimaxTree:
         deck = cribbage.deck.Deck()
         for i in deck.cards:
             whole_deck.append(i)
-        print("Whole deck: ", whole_deck)
+        #print("Whole deck: ", whole_deck)
 
         # Remove known cards from deck
         for i in hand:
@@ -82,25 +94,24 @@ class expectimaxTree:
         for i in sequence:
             whole_deck.remove(i)
 
+        print(root.children)
+        if len(root.children) > 10:
+            print("Something fucked up")
+
         # For each level 2 node, put in prob nodes with legal moves
-        for i in root.children:
+        for cnode in root.children:
             # Get rid of illegal or used cards
             card_deck = deepcopy(whole_deck)
-            if node.card in card_deck:
+            if cnode.getCard() in card_deck:
                 card_deck.remove(node.card)
             for card in card_deck:
-                if node.sumFromPlay + card[0] > 31 and card in card_deck:
+                if cnode.getSumFromPlay() + card[0] > 31 and card in card_deck:
                     card_deck.remove(card)
 
             # Make new nodes in children of current node using prob nodes
             for card in card_deck:
-                newChanceNode = self.chanceNode(card, 1/len(card_deck))
-                i.debug()
-                i.addChild(newChanceNode)
-
-
-
-
+                newChanceNode = chanceNode(card, 1/len(card_deck))
+                cnode.addChild(newChanceNode)
 
     # Searches the expectimax tree and recommends the card to be played
     # 0 for  no risk, 1 for medium risk, 2 for high risk
